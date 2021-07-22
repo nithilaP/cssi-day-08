d771f436b23faa67b7e7df48a84131006dcb91d3
nithilap <nithila@poongo.net>
2021-07-22 18:27:38 +0000

    Writing messages into the database from the UI.

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,3 +1,33 @@
+//Code Taken from Online to Add Code to Database from UI
+/*
+<script>
+  var config = {
+    apiKey: MYAPIKEY,
+    authDomain: MYDOMAIN,
+    databaseURL: MYURL,
+    projectId: MYPROJECT,
+    storageBucket: STORAGEBUCKET,
+    messagingSenderId: SENDERID
+  };
+  firebase.initializeApp(config);
+</script>
+
+<script>
+    function writeNewPost() {
+      var postData = {
+        "Name": "Brett"
+      };
+
+     var newPostKey = firebase.database().ref().push().key;
+
+     var updates = {};
+     updates['/' + newPostKey] = postData;
+
+     return firebase.database().ref().update(updates);
+   }
+</script>
+*/
+
 const getMessages = () => {
    const passCode = document.querySelector("#passcode");
    const messagesRef = firebase.database().ref();
@@ -6,10 +36,10 @@ const getMessages = () => {
     console.log(data);
     for(let key in data){
         console.log(key,data[key]);
-        if (key === passCode.value){
+        if (data[key].passcode === passCode.value){
             console.log("match found");
             const messages = document.querySelector("#message");
-            message.innerHTML = data[key];
+            message.innerHTML = data[key].message;
         }
     }
    })
