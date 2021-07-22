ccc9e4900b2fc52743a479cb4f5ba8d01b9f2406
nithilap <nithila@poongo.net>
2021-07-22 16:46:08 +0000

    Read data from database!

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,4 +1,8 @@
 const getMessages = () => {
    const passCode = document.querySelector("#passcode");
-    console.log("button pressed", passCode.value);
+   const messagesRef = firebase.database().ref();
+   messagesRef.on('value', (snapshot) => { //when messagesRef changes, get value of that function - .val function pulls the value out of the snapshot of the datbase
+    const data = snapshot.val();
+    console.log(data);
+   })
 }
